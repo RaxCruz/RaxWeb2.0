fea6d11a547ec2f4c4f03c581d90f2891697bf92
Rax <peter030910@gmail.com>
2024-02-08 14:48:46 +0800

    test: add skeleton

--- components/CarouselRaxCourseC.tsx
+++ components/CarouselRaxCourseC.tsx
@@ -9,6 +9,7 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel"
 import Image from "next/image";
+import { Suspense } from "react";
 
 export function CarouselRaxCourseC() {
   const myArray = [
@@ -70,8 +71,10 @@ export function CarouselRaxCourseC() {
               <Card className="relative">
                 <p className="absolute font-['ChenYu'] left-2 top-2 text-[2.5rem]">第<span>{index+1}</span>章</p>
                 <CardContent className="flex aspect-square items-center justify-center p-0">
+                  <Suspense fallback={<div className="skeleton w-[400px] h-[400px]"></div>}>
                   <Image src={item.image} width={400} height={400} alt={item.title}/>
                   {/* <span className="text-4xl font-semibold">{index + 1}</span> */}
+                  </Suspense>
                 </CardContent>
               </Card>
             </div>
